70f77f79f14a67b18d568f31c7ddee6f73495f4c
Bharti  Pathak <pathakbharti661@gmail.com>
2026-06-02 20:23:08 +0530

    Fix loop structure in containsDuplicate method

--- Week_1/Day_2/ContainsDuplicates.java
+++ Week_1/Day_2/ContainsDuplicates.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         int count=0;
         int n=nums.length;
-        for (int i =0;i<n;i++){
+        for (int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if (nums[i]==nums[j]){
                     count++;
