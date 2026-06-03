8ccb81caf2bd3576cdce412a4c8eb6d877be3128
Bharti  Pathak <pathakbharti661@gmail.com>
2026-06-03 21:27:18 +0530

    Implement sortedSquares method to return squared sorted array

---
+++ Week_1/Day_3/Sq_of_Sorted_Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int l=nums.length;
+        int[] sq=new int[l];
+        for (int i=0;i<l;i++){
+            sq[i]=nums[i]*nums[i];
+        }
+        for (int i=0;i<l-1;i++){
+            for (int j=0;j<l-i-1;j++){
+                if (sq[j]>sq[j+1]){
+                    int temp=sq[j];
+                    sq[j]=sq[j+1];
+                    sq[j+1]=temp;
+
+                }
+
+                
+            }
+
+        }
+        return sq;
+    }
+}
