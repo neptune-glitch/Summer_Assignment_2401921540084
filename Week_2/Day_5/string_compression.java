a221ff41f76eb561cc7e0aea64557c3addc53be2
Bharti  Pathak <pathakbharti661@gmail.com>
2026-06-14 21:24:30 +0530

    Implement string compression algorithm

---
+++ Week_2/Day_5/string_compression.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        int index = 0;
+        int i = 0;
+
+        while (i < chars.length) {
+
+            char current = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == current) {
+                count++;
+                i++;
+            }
+
+            chars[index++] = current;
+
+            if (count > 1) {
+                String num = String.valueOf(count);
+
+                for (char c : num.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+}
