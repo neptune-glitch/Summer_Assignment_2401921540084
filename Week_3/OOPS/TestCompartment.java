5fd26f5c1b432ae7a34dc8787cfbfe11c81e33ea
Bharti  Pathak <pathakbharti661@gmail.com>
2026-06-22 23:39:21 +0530

    Implement compartment classes and test functionality

---
+++ Week_3/OOPS/TestCompartment.java
@@ -0,0 +1,67 @@
+abstract class Compartment {
+    public abstract String notice();
+}
+
+class FirstClass extends Compartment {
+    @Override
+    public String notice() {
+        return "Notice: First Class Compartment";
+    }
+}
+
+class Ladies extends Compartment {
+    @Override
+    public String notice() {
+        return "Notice: Ladies Compartment";
+    }
+}
+
+class General extends Compartment {
+    @Override
+    public String notice() {
+        return "Notice: General Compartment";
+    }
+}
+
+class Luggage extends Compartment {
+    @Override
+    public String notice() {
+        return "Notice: Luggage Compartment";
+    }
+}
+
+public class TestCompartment {
+    public static void main(String[] args) {
+
+        Compartment[] compartments = new Compartment[10];
+
+        for (int i = 0; i < compartments.length; i++) {
+
+            int random = (int)(Math.random() * 4) + 1;
+
+            switch (random) {
+                case 1:
+                    compartments[i] = new FirstClass();
+                    break;
+
+                case 2:
+                    compartments[i] = new Ladies();
+                    break;
+
+                case 3:
+                    compartments[i] = new General();
+                    break;
+
+                case 4:
+                    compartments[i] = new Luggage();
+                    break;
+            }
+        }
+
+        System.out.println("Compartment Notices:");
+
+        for (Compartment c : compartments) {
+            System.out.println(c.notice());
+        }
+    }
+}
